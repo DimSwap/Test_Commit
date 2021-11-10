12df4b8ab03615c81defa91f6e10ab752b372663
DimSwap <logu123dima@gmail.com>
2021-11-11 00:34:57 +0200

    Добавляем студентов из группы 2(ArrayList в HashMap).Идентифицируем и задаём уникальный хэш-код.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,12 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
+    static int key = 0;
 
     public static void main(String[] args) {
+
         List<String> student = new ArrayList<>();
         student.add("Dima");
         student.add("Damir");
@@ -14,11 +15,23 @@ public class Main {
         student.add("Oleg");
         System.out.println(student);
         student.remove(1);
-      student.remove(2);
+        student.remove(2);
+        System.out.println(student);
         List<String> student_group_2 = new ArrayList<>(List.copyOf(student));
         student_group_2.add("Den");
         student_group_2.add("Maks");
+        System.out.println(student_group_2);
         System.out.println(student_group_2.size());
+        Map<Integer, Object> map = new HashMap<>();
+        for (int i = 0; i < 5; i++) {
+            map.put(i, student_group_2.get(key));
+            key++;
+            System.out.println(map+" hashCode = "+map.hashCode()+";");
+            map.clear();
+
+        }
+
 
     }
+
 }
