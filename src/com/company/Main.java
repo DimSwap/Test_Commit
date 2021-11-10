4068374458dc98374c10610e12da0f4501c6cce8
DimSwap <logu123dima@gmail.com>
2021-11-10 19:29:04 +0200

    Adding students

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
         List<String> student = new ArrayList<>();
-
+        student.add("Dima");
+        student.add("Damir");
+        student.add("Vlad");
+        student.add("Olga");
+        student.add("Oleg");
+        System.out.println(student);
     }
 }
